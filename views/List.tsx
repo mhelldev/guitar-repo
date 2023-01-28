0754d889e63321df26203273dae0ba30fd64f4cd
michael heller <michaelheller007@googlemail.com>
2023-01-28 22:52:50 +0100

    read Data, create scroll view

--- views/List.tsx
+++ views/List.tsx
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {
     Container,
     Alert,
@@ -12,9 +12,11 @@ import {
     Stack,
     Slider,
     Select,
-    CheckIcon, Menu, HamburgerIcon, Pressable, Heading
+    CheckIcon, Menu, HamburgerIcon, Pressable, Heading, VStack, Center, Divider, Link, ScrollView, Badge, Flex
 } from 'native-base';
 import {ViewProperties} from "../App";
+import {SongEntry, Style} from "./CreateEntry";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 type FormData = {
   name: string;
@@ -23,6 +25,25 @@ type FormData = {
 };
 
 export default function List(props: ViewProperties) {
+
+    const [songs, setSongs] = useState<SongEntry[]>()
+
+    const readData = async () => {
+        try {
+            const guitarRepoJsonString = await AsyncStorage.getItem('@guitar-repo')
+            let guitarRepo: SongEntry[] = []
+            if (guitarRepoJsonString) {
+                guitarRepo = JSON.parse(guitarRepoJsonString)
+            }
+            setSongs(guitarRepo)
+        } catch (e) {
+            console.log('error reading file')
+        }
+    }
+
+    useEffect(() => {
+        readData()
+    }, [])
 
   return (
       <Box alignItems="center" marginTop={5}>
@@ -36,7 +57,27 @@ export default function List(props: ViewProperties) {
                   <Menu.Item onPress={() => {props.routeHandler('Create')}}>Create Song</Menu.Item>
               </Menu>
           </Box>
-          <Text>'List!'</Text>
+          <Container alignItems="center" marginBottom={4}>
+              <Flex direction="row"><Badge marginRight={2}>Jazz</Badge><Badge marginRight={2}>Fingerpicking</Badge></Flex>
+          </Container>
+          <ScrollView w={'100%'}>
+              {songs?.map(song =>
+                  <>
+                      <Container alignItems="left" marginLeft={4}>
+                          <Heading>
+                              <Text color="emerald.500">{song.song}</Text>
+                              <Text> {song.artist}</Text>
+                          </Heading>
+                          <Text mt="1" fontWeight="medium">
+                              <Text bold>{song.style}</Text> - {song.progress}%
+                          </Text>
+                          <Link fontWeight="medium">{song.youtube}</Link>
+                          <Link fontWeight="medium">{song.ultimateGuitar}</Link>
+                      </Container>
+                      <Divider my="2"/>
+                  </>
+              )}
+          </ScrollView>
       </Box>
   );
 };
