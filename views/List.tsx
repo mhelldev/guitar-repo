46a611dab9e2683360b9c233d66a2ec96dc575d1
michael heller <michaelheller007@googlemail.com>
2023-01-30 23:38:22 +0100

    change list colors

--- views/List.tsx
+++ views/List.tsx
@@ -60,7 +60,7 @@ export default function List(props: ViewProperties) {
     }, [])
 
   return (
-      <Box alignItems="center" marginTop={5}>
+      <Box alignItems="center" marginTop={5} backgroundColor="red.200">
           <Box w="90%" marginLeft={4} marginY={5} alignItems="left">
               <Menu w="190" trigger={triggerProps => {
                   return <Pressable accessibilityLabel="More options menu" {...triggerProps}>
@@ -72,18 +72,18 @@ export default function List(props: ViewProperties) {
               </Menu>
           </Box>
           <Container alignItems="center" >
-              <Flex direction="row"><Badge marginRight={2} backgroundColor={style === 'Jazz' ? 'emerald.500' : 'gray.100'} onTouchStart={() => style === 'Jazz' ? setStyle(undefined) : setStyle('Jazz')}>Jazz</Badge><Badge marginRight={2} backgroundColor={style === 'Fingerpicking' ? 'emerald.500' : 'gray.100'} onTouchStart={() => style === 'Fingerpicking' ? setStyle(undefined) : setStyle('Fingerpicking')}>Fingerpicking</Badge>
-                  <Badge marginRight={2} backgroundColor={style === 'Blues' ? 'emerald.500' : 'gray.100'} onTouchStart={() => style === 'Blues' ? setStyle(undefined) : setStyle('Blues')}>Blues</Badge><Badge marginRight={2} backgroundColor={style === 'Rock/Pop' ? 'emerald.500' : 'gray.100'} onTouchStart={() => style === 'Rock/Pop' ? setStyle(undefined) : setStyle('Rock/Pop')}>Rock/Pop</Badge></Flex>
+              <Flex direction="row"><Badge marginRight={2} backgroundColor={style === 'Jazz' ? 'red.300' : 'gray.100'} onTouchStart={() => style === 'Jazz' ? setStyle(undefined) : setStyle('Jazz')}>Jazz</Badge><Badge marginRight={2} backgroundColor={style === 'Fingerpicking' ? 'red.300' : 'gray.100'} onTouchStart={() => style === 'Fingerpicking' ? setStyle(undefined) : setStyle('Fingerpicking')}>Fingerpicking</Badge>
+                  <Badge marginRight={2} backgroundColor={style === 'Blues' ? 'red.300' : 'gray.100'} onTouchStart={() => style === 'Blues' ? setStyle(undefined) : setStyle('Blues')}>Blues</Badge><Badge marginRight={2} backgroundColor={style === 'Rock/Pop' ? 'red.300' : 'gray.100'} onTouchStart={() => style === 'Rock/Pop' ? setStyle(undefined) : setStyle('Rock/Pop')}>Rock/Pop</Badge></Flex>
           </Container>
-          <Divider marginBottom={4}/>
+          <Divider marginBottom={4} backgroundColor="gray.700"/>
 
-          <ScrollView w={'100%'}>
+          <ScrollView w={'100%'} >
               {songs?.filter(song => song.style === style || style === undefined).map(song =>
                   <>
                       <Flex direction={'row'}>
                           <Container alignItems="left" marginLeft={6} w={'100%'}>
                               <Heading>
-                                  <Text color="emerald.500">{song.song}</Text>
+                                  <Text color="warmGray.50">{song.song}</Text>
                                   <Text> {song.artist}</Text>
                               </Heading>
                               <Text mt="1" fontWeight="medium">
@@ -93,14 +93,14 @@ export default function List(props: ViewProperties) {
                               <Link fontWeight="medium">{song.ultimateGuitar}</Link>
                           </Container>
                           <Flex direction={'column'}>
-                              <IconButton icon={<Icon as={AntDesign} name="delete" />} borderRadius="full" />
-                              <IconButton icon={<Icon as={AntDesign} name="edit" />} borderRadius="full" onPress={() => {
+                              <IconButton icon={<Icon as={AntDesign} color={'gray.700'} name="delete" />} borderRadius="full" />
+                              <IconButton icon={<Icon as={AntDesign} color={'gray.700'} name="edit" />} borderRadius="full" onPress={() => {
 
                                   props.appStateHandler({route: 'Create', entry: song})
                               }}/>
                           </Flex>
                       </Flex>
-                      <Divider ml={'4%'} width={'92%'} my="2"/>
+                      <Divider ml={'4%'} width={'92%'} my="2" backgroundColor="gray.700"/>
                   </>
               )}
           </ScrollView>
